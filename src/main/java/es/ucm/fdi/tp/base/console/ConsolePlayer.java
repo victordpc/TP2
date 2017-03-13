aebc6d6c3c24708d105d35bcba35f452dd43c0eb
victordpc <victordpc@gmail.com>
2017-03-13 23:06:42 +0100

    Doc

--- src/main/java/es/ucm/fdi/tp/base/console/ConsolePlayer.java
+++ src/main/java/es/ucm/fdi/tp/base/console/ConsolePlayer.java
@@ -10,73 +10,69 @@ import es.ucm.fdi.tp.base.model.GamePlayer;
 import es.ucm.fdi.tp.base.model.GameState;
 
 /**
- * A console player.
- * Can play any game, and offers console users a list of 
- * valid actions to choose from.
+ * A console player. Can play any game, and offers console users a list of valid
+ * actions to choose from.
  */
 public class ConsolePlayer implements GamePlayer {
 
-    private int playerNumber;
-    private String name;
-    private Scanner in;	
+	private Scanner in;
+	private String name;
+	private int playerNumber;
 
-    public ConsolePlayer(String name, Scanner in) {
-        this.name = name;
-        this.in = in;
-        this.playerNumber = -1;
-    }
+	public ConsolePlayer(String name, Scanner in) {
+		this.name = name;
+		this.in = in;
+		this.playerNumber = -1;
+	}
 
-    @Override
-    public void join(int playerNumber) {
-        this.playerNumber = playerNumber;
-    }
+	@Override
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public int getPlayerNumber() {
-    	return playerNumber;
-    }
-    
-    @Override
-    public String getName() {
-        return name;
-    }
+	@Override
+	public int getPlayerNumber() {
+		return playerNumber;
+	}
 
-	public <S extends GameState<S,A>, A extends GameAction<S,A>> A requestAction(S state) {
+	@Override
+	public void join(int playerNumber) {
+		this.playerNumber = playerNumber;
+	}
 
-	    List<A> actions = new ArrayList<A>();
-        actions.addAll(state.validActions(playerNumber));
+	@Override
+	public <S extends GameState<S, A>, A extends GameAction<S, A>> A requestAction(S state) {
 
-    	// displays a menu with all available actions + exit
-    	System.out.println(
-                "Available actions: \n" +
-                "\t0 - exit game");		
-        int i = 0;
-		for (GameAction<?,?> a : actions) {
+		List<A> actions = new ArrayList<A>();
+		actions.addAll(state.validActions(playerNumber));
+
+		// displays a menu with all available actions + exit
+		System.out.println("Available actions: \n" + "\t0 - exit game");
+		int i = 0;
+		for (GameAction<?, ?> a : actions) {
 			System.out.println("\t" + (++i) + " - " + a);
 		}
 
 		// read the user input and carry out action
-        A action = null;
-        while (action == null) {
-            System.out.print("Please type your move index: ");
-            try {
-            	int choice = in.nextInt();
+		A action = null;
+		while (action == null) {
+			System.out.print("Please type your move index: ");
+			try {
+				int choice = in.nextInt();
 
-	            if (choice == 0) {
-	                // user wants to exit
-	                System.out.println("Game exiting by request of " + name);
-	                System.exit(0);
-	            } else if (choice > 0 && choice <= actions.size()) {
-	                action = actions.get(choice - 1);
-	            } else {
-	                System.out.println(
-	                		"Out of range (0 to " + actions.size() 
-	                		+ "); please try again.");
-	            }
-            } catch (InputMismatchException ime) {
-            	System.out.println("Expected an integer; please try again");
-            }
-        }
-        return action;
+				if (choice == 0) {
+					// user wants to exit
+					System.out.println("Game exiting by request of " + name);
+					System.exit(0);
+				} else if (choice > 0 && choice <= actions.size()) {
+					action = actions.get(choice - 1);
+				} else {
+					System.out.println("Out of range (0 to " + actions.size() + "); please try again.");
+				}
+			} catch (InputMismatchException ime) {
+				System.out.println("Expected an integer; please try again");
+			}
+		}
+		return action;
 	}
 }
--- src/main/java/es/ucm/fdi/tp/base/model/GameError.java
+++ src/main/java/es/ucm/fdi/tp/base/model/GameError.java
@@ -2,7 +2,7 @@ package es.ucm.fdi.tp.base.model;
 
 public class GameError extends RuntimeException {
 	private static final long serialVersionUID = 4703354133717328836L;
-	
+
 	public GameError(String msg) {
 		super(msg);
 	}
