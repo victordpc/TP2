7f03ed7320820a3588853aacfb50f56edbc724f8
victordpc <victordpc@gmail.com>
2017-03-22 15:52:10 +0100

    Añadido código proporcionado para P5

--- src/main/java/es/ucm/fdi/tp/base/model/GameError.java
+++ src/main/java/es/ucm/fdi/tp/base/model/GameError.java
@@ -1,10 +1,25 @@
 package es.ucm.fdi.tp.base.model;
 
+/**
+ * Describe illegal game actions or other game errors.
+ */
 public class GameError extends RuntimeException {
 	private static final long serialVersionUID = 4703354133717328836L;
 
+	/**
+	 * A game error
+	 * @param msg that describes the error
+	 */
 	public GameError(String msg) {
 		super(msg);
 	}
 
+	/**
+	 * A game error that wraps an exception
+	 * @param msg that describes the error
+	 * @param cause of the error
+	 */
+	public GameError(String msg, Throwable cause) {
+		super(msg, cause);
+	}
 }
